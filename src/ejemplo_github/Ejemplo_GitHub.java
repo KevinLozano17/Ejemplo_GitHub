3dfcc244c3ced7e55666f6ed47d35f01fb1d5210
kevin <kevin@KEVIN-RUIZD15.mshome.net>
2020-05-13 12:29:53 -0600

    Primer commit del proyecto que muestra saludo

---
+++ src/ejemplo_github/Ejemplo_GitHub.java
@@ -0,0 +1,22 @@
+/*
+Ejemplo creado para subir a GitHub
+ */
+package ejemplo_github;
+
+/**
+ *
+ * @author kevin
+ */
+public class Ejemplo_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos...!");
+        System.out.println("Welcome");
+        System.out.println("Hi...!!!");
+        
+    }
+    
+}
